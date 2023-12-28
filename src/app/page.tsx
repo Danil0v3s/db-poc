53c24611a25d15fa6ca297b296868489e051e35b
Danilo Menezes <danilo.menezes@sky.uk>
2023-12-28 23:10:18 +0000

    search kinda working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,178 @@
-import Image from 'next/image'
+'use client'
+
+import { FormEventHandler, useEffect, useState } from "react"
+import { useDebounce } from "@uidotdev/usehooks"
+
+type Query = {
+  value: string
+  isManual: boolean
+}
+
+type Recommendation = {
+  id: number
+  name: string
+}
+
+type Item = {
+  vending_id: number
+  amount: number
+  price: number
+  vending: Vending
+  cart: Cart
+}
+
+type Cart = {
+  nameid: number
+}
+
+type Vending = {
+  id: number
+  map: string
+  x: number
+  y: number
+  char: {
+    name: string
+    shopCode: number
+  }
+}
+
+async function getSuggestions(query: string): Promise<any> {
+  var res = await fetch(`https://roleta.ragna4th.com/db/i/q/in/${query}`);
+  var data = await res.json();
+
+  return data;
+}
+
+async function search(query: string): Promise<any> {
+  var res = await fetch(`https://api.ragna4th.com/db/market`, {
+    method: 'post',
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      limit: 10,
+      filters: [
+        {
+          field: "nameid",
+          operator: "in",
+          value: `[${query}]`
+        }
+      ],
+      order: {
+        by: "price",
+        direction: "asc"
+      }
+    })
+  })
+  var data = await res.json()
+
+  return data.result.items;
+}
 
 export default function Home() {
+  const _: Recommendation = { id: 0, name: '' };
+
+  const [query, setQuery] = useState<Query>({ isManual: false, value: '' })
+  const [isLoading, setLoading] = useState(false)
+  const [suggestions, setSuggestions] = useState<Recommendation[]>([])
+  const [selectedSuggestion, setSelectedSuggestion] = useState<Recommendation>(_)
+  const [data, setData] = useState<Item[]>([])
+
+  const debouncedSearchTerm = useDebounce(query, 300)
+
+  useEffect(() => {
+    const fn = async () => {
+      if (debouncedSearchTerm.value.length < 3 || !debouncedSearchTerm.isManual) return;
+      const suggestions: Recommendation[] = await getSuggestions(debouncedSearchTerm.value);
+      setSuggestions(suggestions);
+
+      const result = await search(suggestions.map(it => it.id).splice(20).join(","));
+      setData(result);
+    };
+
+    fn();
+  }, [debouncedSearchTerm])
+
+  useEffect(() => {
+    const fn = async () => {
+      if (selectedSuggestion.id > 0) {
+        setQuery({ value: selectedSuggestion.name, isManual: false });
+        const result = await search(`${selectedSuggestion.id}`);
+        setData(result);
+      }
+    };
+
+    fn();
+  }, [selectedSuggestion])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
+        <div className="card w-full">
+          <div className="flex-row w-full pb-2">
+            <SuggestionsComponent
+              onSubmit={e => {
+                e.preventDefault();
+                setSuggestions([]);
+              }}
+              query={query.value}
+              onInputChange={(e) => setQuery({ value: e, isManual: true })}
+              onRecommendationSelected={setSelectedSuggestion}
+              recommendations={suggestions} />
+          </div>
+          <hr />
+          <div className="flex-row pt-2">
+            <table className="w-full">
+              <thead>
+                <th>ITEM</th>
+                <th>PREÇO</th>
+                <th>QUANTIDADE</th>
+                <th>LOCALIZAÇÃO</th>
+                <th>VENDEDOR</th>
+              </thead>
+              <tbody>
+                {
+                  data.map((it, index) => {
+                    return (
+                      <tr key={index}>
+                        <td><img src={`https://roleta.ragna4th.com/db/i/ic/${it.cart.nameid}`} /> {it.cart.nameid}</td>
+                        <td>{it.price}ƶ</td>
+                        <td>{it.amount}</td>
+                        <td>
+                          <p>{it.vending.map} {it.vending.x}, {it.vending.y}</p>
+                          <span>{it.vending.char.shopCode}</span>
+                        </td>
+                        <td>{it.vending.char.name}</td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
+  )
+}
+
+type SuggestionProps = {
+  recommendations: Recommendation[];
+  query: string;
+  onSubmit: FormEventHandler<HTMLFormElement> | undefined;
+  onInputChange: (value: string) => void;
+  onRecommendationSelected: (rec: Recommendation) => void;
+}
+
+function SuggestionsComponent(props: SuggestionProps) {
+  return (
+    <form onSubmit={props.onSubmit}>
+      <input className="w-full" value={props.query} placeholder="Search" onChange={e => props.onInputChange(e.target.value)} />
+      {
+        props.recommendations && props.recommendations.map((it, index) => {
+          return (<p key={index} onClick={() => props.onRecommendationSelected(it)}>{it.name}</p>)
+        })
+      }
+    </form>
   )
 }

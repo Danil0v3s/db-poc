c5b6f6d3dc8fbd166eb4b486131032b8bb935a67
Danilo Lemes <daniloleemes@hotmail.com>
2023-12-29 21:10:27 +0000

    add loading and empty results

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,9 +97,11 @@ export default function Home() {
     const fn = async () => {
       if (debouncedSearchTerm.value.length < 3 || !debouncedSearchTerm.isManual) return;
       const suggestions: Recommendation[] = await getSuggestions(debouncedSearchTerm.value);
-      setSuggestions(suggestions);
+      setSuggestions(suggestions.slice(0, 20));
 
+      setLoading(true);
       const result = await search(suggestions.map(it => it.id).slice(0, 20).join(","), quantity);
+      setLoading(false);
       setData(result);
     };
 
@@ -109,9 +111,11 @@ export default function Home() {
   useEffect(() => {
     const fn = async () => {
       if (selectedSuggestion.id > 0) {
-        setSuggestions([])
+        setSuggestions([]);
         setQuery({ value: selectedSuggestion.name, isManual: false });
+        setLoading(true);
         const result = await search(`${selectedSuggestion.id}`, quantity);
+        setLoading(false);
         setData(result);
       }
     };
@@ -122,7 +126,9 @@ export default function Home() {
 
   useEffect(() => {
     const fn = async () => {
+      setLoading(true);
       var items = await search("", 10);
+      setLoading(false);
       setData(items);
     };
 
@@ -159,8 +165,29 @@ export default function Home() {
               </thead>
               <tbody >
                 {
-                  data.map((it, index) => {
-                    // ??????
+                  isLoading && (
+                    <tr className="w-full bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                      <th scope="row" colSpan={6} className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white justify-center">
+                        <div role="status">
+                          <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                          </svg>
+                          <span className="sr-only">Loading...</span>
+                        </div>
+                      </th>
+                    </tr>
+                  )
+                }
+                {
+                  data.length == 0 && !isLoading && (
+                    <tr className="w-full bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                      <th scope="row" colSpan={6} className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Sua pesquisa não retornou resultados</th>
+                    </tr>
+                  )
+                }
+                {
+                  data.length > 0 && !isLoading && data.map((it, index) => {
                     const item = itemNames[it.cart.nameid.toString()];
                     const card0 = itemNames[it.cart.card0.toString()];
                     const card1 = itemNames[it.cart.card1.toString()];
@@ -224,8 +251,8 @@ type SuggestionProps = {
 
 function SuggestionsComponent(props: SuggestionProps) {
   return (
-    <form onSubmit={props.onSubmit}>
-      <div className="mb-6 flex">
+    <form onSubmit={props.onSubmit} className="relative">
+      <div className="mb-2 flex">
         <input value={props.query}
           placeholder="Search"
           onChange={e => props.onInputChange(e.target.value)}
@@ -246,9 +273,17 @@ function SuggestionsComponent(props: SuggestionProps) {
         </select>
       </div>
       {
-        props.recommendations && props.recommendations.map((it, index) => {
-          return (<p key={index} onClick={() => props.onRecommendationSelected(it)}>{it.name}</p>)
-        })
+        props.recommendations.length > 0 && (
+          <div className="absolute w-full bg-gray-600 border border-gray-300 text-white-900 text-sm rounded-lg flex group flex-col bg-opacity-90">
+            {
+              props.recommendations.map((it, index) => {
+                return (
+                  <div className="px-2 py-2" key={index} onClick={() => props.onRecommendationSelected(it)}>{it.name}</div>
+                )
+              })
+            }
+          </div>
+        )
       }
     </form>
   )

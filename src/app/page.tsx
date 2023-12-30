9f351e192a5bd6b440828f0ce724af4a006ba243
Danilo Lemes <daniloleemes@hotmail.com>
2023-12-30 13:30:04 +0000

    scrap db for item history

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { FormEventHandler, useEffect, useState } from "react"
 import { useDebounce } from "@uidotdev/usehooks"
 import itemNames, { ItemNameSlot } from "./id_name_slots"
+import { load } from 'cheerio';
 
 type Query = {
   value: string
@@ -42,6 +43,13 @@ type Vending = {
   }
 }
 
+type ItemHistory = {
+  nameid: number,
+  cheapestAvailable: number,
+  cheapestSold: number,
+  highestSold: number,
+  quantitySold: number,
+}
 
 async function getSuggestions(query: string): Promise<any> {
   var res = await fetch(`https://roleta.ragna4th.com/db/i/q/in/${query}`);
@@ -50,7 +58,7 @@ async function getSuggestions(query: string): Promise<any> {
   return data;
 }
 
-async function search(nameIds: string, items: number, offset: number = 0): Promise<any> {
+async function search(nameIds: string, items: number, offset: number = 0): Promise<Item[]> {
   const filters = []
   if (nameIds.length > 0) {
     filters.push({
@@ -80,16 +88,40 @@ async function search(nameIds: string, items: number, offset: number = 0): Promi
   return data.result.items;
 }
 
+async function scrap(nameid: number): Promise<ItemHistory> {
+  const data = await (await fetch(`https://db.ragna4th.com/item/${nameid}`)).text()
+  const $ = load(data);
+
+  const cells = $('div.nk-iv-wg3-sub');
+  const values = cells.map((index, child) => {
+    const title = $(child).find("center")[0];
+    const value = $(child).find("center")[1];
+    const subtitle = $(child).find("center")[2];
+
+    return $(value).text()
+  }).toArray();
+
+  return {
+    nameid,
+    cheapestAvailable: parseInt(values[0].replaceAll(".", "").replaceAll("ƶ", "")),
+    cheapestSold: parseInt(values[1].replaceAll(".", "").replaceAll("ƶ", "")),
+    highestSold: parseInt(values[2].replaceAll(".", "").replaceAll("ƶ", "")),
+    quantitySold: parseInt(values[3].replaceAll(".", "").replaceAll("ƶ", ""))
+  }
+}
+
 export default function Home() {
   const _: Recommendation = { id: 0, name: '' };
 
   const [query, setQuery] = useState<Query>({ isManual: false, value: '' })
   const [isLoading, setLoading] = useState(false)
+  const [isLoadingHistory, setLoadingHistory] = useState(false)
   const [suggestions, setSuggestions] = useState<Recommendation[]>([])
   const [selectedSuggestion, setSelectedSuggestion] = useState<Recommendation>(_)
   const [data, setData] = useState<Item[]>([])
   const [quantity, setQuantity] = useState<number>(10)
-  const [copy, setCopy] = useState<boolean>(true)
+  const [uniqueIds, setUniqueIds] = useState<number[]>([])
+  const [itemHistory, setItemHistory] = useState<ItemHistory[]>([]);
 
   const debouncedSearchTerm = useDebounce(query, 300)
 
@@ -101,6 +133,15 @@ export default function Home() {
 
       setLoading(true);
       const result = await search(suggestions.map(it => it.id).slice(0, 20).join(","), quantity);
+      let uniqueIds: number[] = [];
+      result.reduce((result, item, index, array) => {
+        if (!result.includes(item.cart.nameid)) {
+          result.push(item.cart.nameid);
+        }
+
+        return result;
+      }, uniqueIds);
+      setUniqueIds(uniqueIds);
       setLoading(false);
       setData(result);
     };
@@ -123,7 +164,6 @@ export default function Home() {
     fn();
   }, [selectedSuggestion, quantity])
 
-
   useEffect(() => {
     const fn = async () => {
       setLoading(true);
@@ -134,6 +174,16 @@ export default function Home() {
 
     fn();
   }, [])
+
+  useEffect(() => {
+    const fn = async () => {
+      setLoadingHistory(true);
+      const itemsSellingHistory = await Promise.all(uniqueIds.map(it => scrap(it)));
+      setLoadingHistory(false);
+      setItemHistory(itemsSellingHistory);
+    }
+    fn();
+  }, [uniqueIds]);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
@@ -153,6 +203,8 @@ export default function Home() {
             />
           </div>
 
+          {/* Items */}
+          <p className="text-4xl text-gray-900 dark:text-white pb-4 pt-4">Resultado</p>
           <div className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
               <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -227,6 +279,65 @@ export default function Home() {
                           </div>
                         </td>
                         <td className="px-6 py-4">{it.vending.char.name}</td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </table>
+          </div>
+
+          {/* History */}
+          <p className="text-4xl text-gray-900 dark:text-white pb-4 pt-8">Histórico de vendas</p>
+          <div className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <th scope="col" className="px-6 py-3">ITEM</th>
+                <th scope="col" className="px-6 py-3">MENOR PREÇO DISP.</th>
+                <th scope="col" className="px-6 py-3">MENOR PREÇO VENDIDO</th>
+                <th scope="col" className="px-6 py-3">MAIOR PREÇO VENDIDO</th>
+                <th scope="col" className="px-6 py-3">NUM. VENDAS</th>
+              </thead>
+              <tbody >
+                {
+                  isLoadingHistory && (
+                    <tr className="w-full bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                      <th scope="row" colSpan={6} className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white justify-center">
+                        <div role="status">
+                          <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                          </svg>
+                          <span className="sr-only">Loading...</span>
+                        </div>
+                      </th>
+                    </tr>
+                  )
+                }
+                {
+                  itemHistory.length == 0 && !isLoadingHistory && (
+                    <tr className="w-full bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                      <th scope="row" colSpan={6} className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Sua pesquisa não retornou resultados</th>
+                    </tr>
+                  )
+                }
+                {
+                  itemHistory.length > 0 && !isLoadingHistory && itemHistory.map((it, index) => {
+                    const item = itemNames[it.nameid.toString()];
+
+                    return (
+                      <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                          <div className="flex gap-3">
+                            <img src={`https://roleta.ragna4th.com/db/i/ic/${it.nameid}`} height={24} width={24} alt={item.name} />
+                            <p className="text-left">{item.name} {parseInt(item.slots) > 0 ? `[${item.slots}]` : ""}</p>
+                          </div>
+                        </th>
+
+                        <td className="px-6 py-4">{new Intl.NumberFormat('pt-BR').format(it.cheapestAvailable)}ƶ</td>
+                        <td className="px-6 py-4">{new Intl.NumberFormat('pt-BR').format(it.cheapestSold)}ƶ</td>
+                        <td className="px-6 py-4">{new Intl.NumberFormat('pt-BR').format(it.highestSold)}ƶ</td>
+                        <td className="px-6 py-4">{new Intl.NumberFormat('pt-BR').format(it.quantitySold)}</td>
                       </tr>
                     )
                   })
